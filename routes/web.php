8acef03a12257abe1e5542e9840d1354a7b3febd
raxit jat <raxitjat@logisticinfotech.co.in>
2020-03-12 11:56:59 +0530

    email verification

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('post','PostController');
